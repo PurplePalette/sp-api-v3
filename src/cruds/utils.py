9b7f717b2106fa711f0cd316be2eed99cbacd783
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-01-27 06:07:14 +0900

    🌱 Add crud utils

---
+++ src/cruds/utils.py
@@ -0,0 +1,86 @@
+from sqlalchemy.engine import Result
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.expression import true
+from src.database.objects.user import User as UserObject
+from typing import Optional, TypeVar, Any
+from sqlalchemy import select
+from fastapi_cloudauth.firebase import FirebaseClaims
+from abc import ABCMeta
+
+
+class MustHaveName(metaclass=ABCMeta):
+    """名前を持つオブジェクトを表す基底クラス"""
+
+    name: str
+
+
+T = TypeVar("T", bound=MustHaveName)
+
+async def get_first_item_or_error(
+    db: AsyncSession, statement: Any, error: HTTPException
+) -> T:
+    """データベースに指定された要素が存在すれば取得、なければエラー"""
+    resp: Result = await db.execute(statement)
+    obj_db: Optional[T] = resp.scalars().first()
+    if obj_db is None:
+        raise error
+    return obj_db
+
+
+async def get_first_item_or_404(
+    db: AsyncSession,
+    statement: Any,
+) -> T:
+    """データベースに指定された要素が存在すれば取得、なければ404"""
+    resp: T = await get_first_item_or_error(
+        db, statement, HTTPException(status_code=404, detail="Not found")
+    )
+    return resp
+
+
+async def get_first_item_or_403(
+    db: AsyncSession,
+    statement: Any,
+) -> T:
+    """データベースに指定された要素が存在すれば取得、なければ403"""
+    resp: T = await get_first_item_or_error(
+        db, statement, HTTPException(status_code=403, detail="Forbidden")
+    )
+    return resp
+
+
+async def get_user_or_404(
+    db: AsyncSession,
+    user: FirebaseClaims,
+) -> UserObject:
+    """データベースに指定されたユーザーが存在すれば取得、なければ404"""
+    user_db: UserObject = await get_first_item_or_404(
+        db, select(UserObject).filter(UserObject.display_id == user["user_id"])
+    )
+    return user_db
+
+
+async def get_admin_or_403(
+    db: AsyncSession,
+    user: FirebaseClaims,
+) -> UserObject:
+    """データベースに指定された管理者ユーザーが存在すれば取得、なければ404"""
+    user_db: UserObject = await get_first_item_or_403(
+        db,
+        select(UserObject).filter(
+            UserObject.display_id == user["user_id"], UserObject.is_admin == true()
+        ),
+    )
+    return user_db
+
+
+async def not_exist_or_409(
+    db: AsyncSession,
+    statement: Any
+) -> None:
+    """データベースに指定された要素が存在すれば409"""
+    resp: Result = await db.execute(statement)
+    obj_db: bool = resp.scalars().first()
+    if obj_db:
+        raise HTTPException(status_code=409, detail="Conflict")
