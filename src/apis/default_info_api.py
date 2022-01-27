4dba9950f8fd8f05f899e0f680f33a97447eb986
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-01-27 09:54:40 +0900

    🚧 ✨ Implement /info endpoint

--- src/apis/default_info_api.py
+++ src/apis/default_info_api.py
@@ -1,23 +1,27 @@
 # coding: utf-8
 
-from typing import Dict, List  # noqa: F401
+import asyncio
 
-from fastapi import (  # noqa: F401
-    APIRouter,
-    Body,
-    Cookie,
-    Depends,
-    Form,
-    Header,
-    Path,
-    Query,
-    Response,
-    Security,
-)
+from fastapi import APIRouter
+from fastapi_pagination import Page, Params
+from fastapi_pagination.ext.async_sqlalchemy import paginate
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.apis.depends import dependsDatabase
-from src.models.extra_models import TokenModel  # noqa: F401
+from src.database.objects.background import Background as BackgroundObject
+from src.database.objects.effect import Effect as EffectObject
+from src.database.objects.engine import Engine as EngineObject
+from src.database.objects.level import Level as LevelObject
+from src.database.objects.particle import Particle as ParticleObject
+from src.database.objects.skin import Skin as SkinObject
+from src.models.default_search import defaultSearch, levelSearch
 from src.models.server_info import ServerInfo
+from src.models.server_info_backgrounds import ServerInfoBackgrounds
+from src.models.server_info_effects import ServerInfoEffects
+from src.models.server_info_engines import ServerInfoEngines
+from src.models.server_info_levels import ServerInfoLevels
+from src.models.server_info_particles import ServerInfoParticles
+from src.models.server_info_skins import ServerInfoSkins
 
 router = APIRouter()
 
@@ -29,10 +33,36 @@ router = APIRouter()
     },
     tags=["default_info"],
     summary="Get default server info",
+    response_model=ServerInfo,
 )
 async def get_server_info(
     db: AsyncSession = dependsDatabase,
 ) -> ServerInfo:
     """It returns small list of all infos registered in this server.
     (It should be trimmed if the server has too many items)"""
-    ...
+    stubPage: Page = Page(items=[], total=0, page=1, size=1)
+    getSize: Params = Params(page=1, size=5)
+    levels: Page[LevelObject] = stubPage
+    skins: Page[SkinObject] = stubPage
+    backgrounds: Page[BackgroundObject] = stubPage
+    effects: Page[EffectObject] = stubPage
+    particles: Page[ParticleObject] = stubPage
+    engines: Page[EngineObject] = stubPage
+    levels, skins, backgrounds, effects, particles, engines = await asyncio.gather(
+        paginate(db, select(LevelObject), getSize),
+        paginate(db, select(SkinObject), getSize),
+        paginate(db, select(BackgroundObject), getSize),
+        paginate(db, select(EffectObject), getSize),
+        paginate(db, select(ParticleObject), getSize),
+        paginate(db, select(EngineObject), getSize),
+    )  # type: ignore
+    return ServerInfo(
+        levels=ServerInfoLevels(items=levels.items, search=levelSearch),
+        skins=ServerInfoSkins(items=skins.items, search=defaultSearch),
+        backgrounds=ServerInfoBackgrounds(
+            items=backgrounds.items, search=defaultSearch
+        ),
+        effects=ServerInfoEffects(items=effects.items, search=defaultSearch),
+        particles=ServerInfoParticles(items=particles.items, search=defaultSearch),
+        engines=ServerInfoEngines(items=engines.items, search=defaultSearch),
+    )
---
+++ src/models/default_search.py
@@ -0,0 +1,146 @@
+from src.models.search import Search
+from src.models.search_text_option import SearchTextOption
+from src.models.search_slider_option import SearchSliderOption
+from src.models.search_toggle_option import SearchToggleOption
+from src.models.search_select_option import SearchSelectOption
+
+
+# 譜面以外のエンドポイント
+defaultSearch: Search = {
+    "options": [
+        # KEYWORDS
+        SearchTextOption(
+            query="keywords",
+            name="text",
+            type="text",
+            placeholder="Search text",
+        ),
+        # AUTHOR
+        SearchTextOption(
+            query="author",
+            name="text",
+            type="text",
+            placeholder="AUTHOR",
+        ),
+        # SORT
+        SearchSelectOption(
+            query="sort",
+            name="sort",
+            type="select",
+            default=0,
+            values=["updated_time", "created_time"],
+        ),
+        # ORDER
+        SearchSelectOption(
+            query="order",
+            name="order",
+            type="select",
+            default=0,
+            values=["desc", "asc"],
+        ),
+        # STATUS
+        SearchSelectOption(
+            query="status",
+            name="status",
+            type="select",
+            default=0,
+            values=["any", "testing"],
+        ),
+        # RANDOM
+        SearchToggleOption(
+            query="random",
+            name="random",
+            type="toggle",
+            default=False,
+        ),
+    ]
+}
+
+# 譜面のエンドポイント
+levelSearch: Search = {
+    "options": [
+        # KEYWORDS
+        SearchTextOption(
+            query="keywords",
+            name="text",
+            type="text",
+            placeholder="Search text",
+        ),
+        # AUTHOR
+        SearchTextOption(
+            query="author",
+            name="text",
+            type="text",
+            placeholder="AUTHOR",
+        ),
+        # SORT
+        SearchSelectOption(
+            query="sort",
+            name="sort",
+            type="select",
+            default=0,
+            values=["updated_time", "created_time"],
+        ),
+        # ORDER
+        SearchSelectOption(
+            query="order",
+            name="order",
+            type="select",
+            default=0,
+            values=["desc", "asc"],
+        ),
+        # RATING_MIN
+        SearchSliderOption(
+            query="rating_min",
+            name="rating_min",
+            type="slider",
+            default=1,
+            min=1,
+            max=50,
+            step=1,
+            display="number",
+        ),
+        # RATING_MAX
+        SearchSliderOption(
+            query="rating_max",
+            name="rating_max",
+            type="slider",
+            default=50,
+            min=1,
+            max=50,
+            step=1,
+            display="number",
+        ),
+        # GENRE
+        SearchSelectOption(
+            query="genre",
+            name="genre",
+            type="select",
+            default=0,
+            values=["any", "vocaloid", "jpop", "anime", "general"],
+        ),
+        # STATUS
+        SearchSelectOption(
+            query="status",
+            name="status",
+            type="select",
+            default=0,
+            values=["any", "testing", "played", "unplayed", "liked", "mylisted"],
+        ),
+        # LENGTH
+        SearchSelectOption(
+            query="length",
+            name="length",
+            type="select",
+            default=0,
+            values=["any", "short", "long", "very_short", "very_long"],
+        ),
+        # RANDOM
+        SearchToggleOption(
+            query="random",
+            name="random",
+            type="toggle",
+            default=False,
+        ),
+    ]
+}
