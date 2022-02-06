da2ff3c77941cae6a1390e3f13b900309e97419c
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-02-06 13:30:58 +0900

    🗃️ Add synced engine

--- src/database/db.py
+++ src/database/db.py
@@ -1,12 +1,18 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from src.database.config import ASYNC_DB_URL
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
+from src.database.config import ASYNC_DB_URL, DB_URL
 
+# 普通のリクエストで使う 非同期エンジン
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
     autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
 )
+
+# SEEDやTESTでのみ使う 同期エンジン
+engine = create_engine(DB_URL, echo=True)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=Session)
 
 Base = declarative_base()
 
@@ -14,3 +20,7 @@ Base = declarative_base()
 async def get_db() -> sessionmaker:
     async with async_session() as session:
         yield session
+
+
+def get_sync_db() -> sessionmaker:
+    return scoped_session(session)
