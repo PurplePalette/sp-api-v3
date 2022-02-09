a5d54d23820de60952b73df522c9d85663bce2a5
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-02-09 20:08:35 +0900

    🔧 Update database seeding script

---
+++ seeder.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+import os
+from sqlalchemy_seed import (
+    load_fixtures,
+    load_fixture_files,
+)
+from src.database.db import Base, get_sync_db  # noqa: F401
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+
+"""
+Fix UnicodeDecodeError with monkey patch
+
+From
+https://qiita.com/ousttrue/items/527a9c3045f710806aa9
+"""
+
+
+def patch_open():
+    import builtins
+
+    __original = open
+
+    def __open(
+        file,
+        mode="r",
+        buffering=-1,
+        encoding=None,
+        errors=None,
+        newline=None,
+        closefd=True,
+        opener=None,
+    ):
+        if "b" not in mode and not encoding:
+            encoding = "utf-8"
+        return __original(
+            file, mode, buffering, encoding, errors, newline, closefd, opener
+        )
+
+    builtins.open = __open
+
+
+def main():
+    patch_open()
+    print("Seeding database...")
+    path: str = "development" if os.environ.get("IS_DEV") else "production"
+    fixtures = load_fixture_files(os.path.join("seeds", path), ["announces.yaml"])
+    session = get_sync_db()
+    load_fixtures(session, fixtures)
+    print("Now database seeded!")
+
+
+if __name__ == "__main__":
+    main()
