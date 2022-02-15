0c4d96fa324734a97eb6cd75a78affdfa4bbbb2a
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-02-16 02:48:52 +0900

    🎨 Add AbstractCrud

---
+++ src/cruds/abstract.py
@@ -0,0 +1,34 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi_cloudauth.firebase import FirebaseClaims
+
+from models.search_query import SearchQueries
+
+
+class AbstractCrud(metaclass=ABCMeta):
+    """
+    CRUD抽象クラス
+    """
+
+    @abstractmethod
+    async def add(self, db: AsyncSession, model: Any, auth: FirebaseClaims) -> Any:
+        pass
+
+    @abstractmethod
+    async def edit(
+        self, db: AsyncSession, name: str, model: Any, auth: FirebaseClaims
+    ) -> Any:
+        pass
+
+    @abstractmethod
+    async def delete(self, db: AsyncSession, name: str, auth: FirebaseClaims) -> Any:
+        return None
+
+    @abstractmethod
+    async def get(self, db: AsyncSession, name: str, localization: str) -> Any:
+        pass
+
+    @abstractmethod
+    async def list(self, db: AsyncSession, page: int, queries: SearchQueries) -> Any:
+        pass
