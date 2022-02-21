a566b30c1ca502a3c69b5f5404689dcad2162481
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-02-21 09:07:13 +0900

    🐛 Ignore warning of transfer.py

--- transfer.py
+++ transfer.py
@@ -144,6 +144,6 @@ if __name__ == "__main__":
     import platform
 
     if platform.system() == "Windows":
-        policy = asyncio.WindowsSelectorEventLoopPolicy()
+        policy = asyncio.WindowsSelectorEventLoopPolicy()  # type: ignore
         asyncio.set_event_loop_policy(policy)  # type: ignore
     asyncio.run(main())
