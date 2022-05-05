055d451f02f896616d1ff896e974f0746adbfc63
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-05-05 09:37:30 +0900

    Fix: Fix ImportError

--- src/database/bucket.py
+++ src/database/bucket.py
@@ -1,11 +1,14 @@
+from typing import TYPE_CHECKING
 import boto3
-import botostubs
 from src.config import S3_BUCKET, S3_ENDPOINT, S3_KEY, S3_SECRET
 
+if TYPE_CHECKING:
+    import botostubs
 
-def get_bucket() -> botostubs.S3.S3Resource.Bucket:
+
+def get_bucket() -> "botostubs.S3.S3Resource.Bucket":
     """Boto3インスタンスを作成し、S3バケットを返します"""
-    s3: botostubs.S3.S3Resource = boto3.resource(
+    s3: "botostubs.S3.S3Resource" = boto3.resource(
         "s3",
         endpoint_url=S3_ENDPOINT,
         aws_access_key_id=S3_KEY,
