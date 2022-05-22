df6e7d3a94d7387d43a88baa5f947a9ae1ea6099
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-05-22 18:23:46 +0900

    Revert: 2ba512aef121aca69315f0adaa60bad67c90bfb2

--- src/cruds/defaults/background.py
+++ src/cruds/defaults/background.py
@@ -1,13 +1,13 @@
 import asyncio
 from typing import Union
 
-from cruds.defaults.defaults_abstract import AbstractCrud
 from fastapi import HTTPException
 from fastapi_pagination import Page, Params
 from fastapi_pagination.ext.async_sqlalchemy import paginate
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
+from src.cruds.defaults.abstract import AbstractCrud
 from src.cruds.utils import (
     db_to_resp,
     get_first_item_or_404,
